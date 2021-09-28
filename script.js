b5b6ea3baec011038dcaea777213ca5d7c820cf0
gavrilovaei <gavrilova-e@yandex.ru>
2021-09-28 23:06:26 +0300

    done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btns = document.querySelectorAll(".filter-btns__item");
+const select = document.getElementById('options');
 const content = document.querySelectorAll('.filter-content img');
 
 for (let btn of btns) {
@@ -13,9 +14,8 @@ for (let btn of btns) {
 
 function filterImages(sel) {
    for (let img of content) {
-      if (!img.classList.contains(sel)) {
-         img.style.display = "none";
-      } else {
+      img.style.display = "none";
+      if (img.classList.contains(sel)) {
          img.style.display = "block";
       };
 
@@ -26,6 +26,13 @@ function filterImages(sel) {
             btn.classList.remove('active');
          };
       };
-   }
+   };
+   select.value = sel;
 };
 // функция, запускающаяся по клику на кнопку будет обходить коллекцию изображений и проверять есть ли у изображения конкретный класс. У classList есть свойство contains, которое принимает конккретный класс и возвращает либо trye либо fals, в зависимости от того есть ли этот класс у элемента. Так же нужно перебрать все кнопки, для них тоже запустить проверку: убрать активный класс если нет класса соответствующего полученному sel, в противном случае активный класс добавить.
+
+select.addEventListener("change", () => {
+   filterImages(select.value);
+});
+
+// для моб версии нужно навесить сдушатель с событием change на celect. событие chenge для select срабатывает при изменении его value(Событие change срабатывает по окончании изменения элемента.Для текстовых <input> это означает, что событие происходит при потере фокуса.Пока мы печатаем в текстовом поле в примере ниже, событие не происходит. Но когда мы перемещаем фокус в другое место, например, нажимая на кнопку, то произойдёт событие change.Для других элементов: select, input type=checkbox/radio событие запускается сразу после изменения значения).
